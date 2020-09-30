85ad164823aee690c43b2c13673e863537fba05c
Henrique Barros <luiz.henrique.lhbs@gmail.com>
2020-09-30 15:57:39 -0400

    Initial commit

---
+++ Assets/WeatherRequest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+public class WeatherRequest : MonoBehaviour
+{
+    private string url = "http://192.168.31.31:8001/api/professores";
+    Text temperature;
+
+    public void GetWeather()
+    {
+        StartCoroutine(MakeWeatherRequest());
+    }
+
+    IEnumerator MakeWeatherRequest ()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        } else
+        {
+            var weather = JsonConvert.DeserializeObject<WeatherResponse>(request.downloadHandler.text);
+
+            temperature = GameObject.Find("temp").GetComponent<Text>();
+            temperature.text = weather.main.temp.ToString();
+        }
+    }
+}
